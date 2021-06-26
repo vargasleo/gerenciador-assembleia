2544e51892457d814d1feb90022dc9acfd03615a
Leonardo Vargas Schilling <leonardo.schilling@cwi.com.br>
2021-06-25 22:55:10 -0300

    fix unit test

--- src/test/java/com/vargas/leo/gerenciadorassembleia/service/RegisterVoteServiceTest.java
+++ src/test/java/com/vargas/leo/gerenciadorassembleia/service/RegisterVoteServiceTest.java
@@ -1,8 +1,12 @@
 package com.vargas.leo.gerenciadorassembleia.service;
 
 import com.vargas.leo.gerenciadorassembleia.controller.request.VoteRequest;
-import com.vargas.leo.gerenciadorassembleia.domain.*;
+import com.vargas.leo.gerenciadorassembleia.domain.User;
+import com.vargas.leo.gerenciadorassembleia.domain.Vote;
+import com.vargas.leo.gerenciadorassembleia.domain.VotingPowerDTO;
+import com.vargas.leo.gerenciadorassembleia.domain.VotingSession;
 import com.vargas.leo.gerenciadorassembleia.domain.enums.VotingOption;
+import com.vargas.leo.gerenciadorassembleia.domain.enums.VotingPower;
 import com.vargas.leo.gerenciadorassembleia.domain.enums.VotingSessionStatus;
 import com.vargas.leo.gerenciadorassembleia.exception.BusinessException;
 import com.vargas.leo.gerenciadorassembleia.exception.NotFoundException;
@@ -39,6 +43,9 @@ public class RegisterVoteServiceTest {
 
     @Mock
     private VotingSessionValidator votingSessionValidator;
+
+    @Mock
+    private CpfValidationService cpfValidationService;
 
     private final Integer mockUserId = 1;
     private final Integer mockVotingSessionId = 1;
@@ -285,12 +292,15 @@ public class RegisterVoteServiceTest {
                 .status(VotingSessionStatus.open)
                 .build();
 
+        VotingPowerDTO votingPowerDTO = VotingPowerDTO.builder().status(VotingPower.ABLE_TO_VOTE).build();
+
         when(userRepository.findById(mockUserId))
                 .thenReturn(Optional.of(user));
         when(votingSessionRepository.findById(mockVotingSessionId))
                 .thenReturn(Optional.of(votingSession));
         when(voteRepository.findByUserIdAndVotingSessionId(mockUserId, mockVotingSessionId))
                 .thenReturn(Optional.empty());
+        when(cpfValidationService.isAllowedToVote(any())).thenReturn(votingPowerDTO);
 
         Vote result = registerVoteService.register(request);
 
@@ -302,6 +312,7 @@ public class RegisterVoteServiceTest {
         verify(userRepository).findById(mockUserId);
         verify(votingSessionRepository).findById(mockVotingSessionId);
         verify(voteRepository).findByUserIdAndVotingSessionId(mockUserId, mockVotingSessionId);
+        verify(cpfValidationService).isAllowedToVote(any());
         verify(voteRepository).save(any(Vote.class));
     }
 
@@ -320,12 +331,15 @@ public class RegisterVoteServiceTest {
 
         votingSession.setStatus(VotingSessionStatus.open);
 
+        VotingPowerDTO votingPowerDTO = VotingPowerDTO.builder().status(VotingPower.ABLE_TO_VOTE).build();
+
         when(userRepository.findById(mockUserId))
                 .thenReturn(Optional.of(user));
         when(votingSessionRepository.findById(mockVotingSessionId))
                 .thenReturn(Optional.of(votingSession));
         when(voteRepository.findByUserIdAndVotingSessionId(mockUserId, mockVotingSessionId))
                 .thenReturn(Optional.empty());
+        when(cpfValidationService.isAllowedToVote(any())).thenReturn(votingPowerDTO);
 
         Vote result = registerVoteService.register(request);
 
@@ -337,7 +351,51 @@ public class RegisterVoteServiceTest {
         verify(userRepository).findById(mockUserId);
         verify(votingSessionRepository).findById(mockVotingSessionId);
         verify(voteRepository).findByUserIdAndVotingSessionId(mockUserId, mockVotingSessionId);
+        verify(cpfValidationService).isAllowedToVote(any());
         verify(voteRepository).save(any(Vote.class));
+    }
+
+    @Test(expected = BusinessException.class)
+    public void shouldThrowExceptionWhenAllValidButCpfNotAllowedToVote() {
+        request.setUserId(mockUserId);
+        request.setVotingSessionId(mockVotingSessionId);
+        request.setVote(mockVotingOptionNo);
+
+
+        User user = User.builder().build();
+
+        VotingSession votingSession = VotingSession.builder()
+                .status(VotingSessionStatus.open)
+                .build();
+
+        votingSession.setStatus(VotingSessionStatus.open);
+
+        VotingPowerDTO votingPowerDTO = VotingPowerDTO.builder().build();
+
+        when(userRepository.findById(mockUserId))
+                .thenReturn(Optional.of(user));
+        when(votingSessionRepository.findById(mockVotingSessionId))
+                .thenReturn(Optional.of(votingSession));
+        when(voteRepository.findByUserIdAndVotingSessionId(mockUserId, mockVotingSessionId))
+                .thenReturn(Optional.empty());
+        when(cpfValidationService.isAllowedToVote(any())).thenReturn(votingPowerDTO);
+
+        try {
+            registerVoteService.register(request);
+        } catch (BusinessException e) {
+            assertEquals("cpf.is.unable.to.vote", e.getMessage());
+
+            verify(userRepository).findById(mockUserId);
+            verify(votingSessionRepository).findById(mockVotingSessionId);
+            verify(voteRepository).findByUserIdAndVotingSessionId(mockUserId, mockVotingSessionId);
+            verify(cpfValidationService).isAllowedToVote(any());
+            verify(voteRepository, never()).save(any(Vote.class));
+
+            throw e;
+        }
+
+
+
     }
 
 }
